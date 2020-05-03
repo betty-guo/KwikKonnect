5e319d38751db436055f7c9f189f6c930b755643
Aphexis <stephxca@gmail.com>
2020-05-03 00:47:25 -0400

    add info command, clean up matching

--- index.js
+++ index.js
@@ -4,34 +4,27 @@ const app = express()
 const port = 3000
 const { WebClient } = require('@slack/web-api');
 
+// PUT AUTH TOKEN HERE
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.post('/', async (req, res) => {
-  // var headers = {
-  //   'Content-Type': 'application/json',
-  //   'Authorization': 'Bearer xoxb-1124198854688-1099012539237-kARJEPLEMg8eBEWUXFRTMBlR'
-  // }
-  //
-  // var options = {
-  //   uri: "https://slack.com/api/chat.postMessage",
-  //   method: 'POST',
-  //   headers: headers,
-  //   json: true,
-  //   body: {
-  //     text: 'Hello, world!',
-  //     channel: 'C012YGSHMFD'
-  //   }
-  // }
+
+app.post('/info', async (req, res) => {
+  // basic info about KiwkKonnect
+  res.send("Hey there, I'm KwikKonnect! I'm here to help you meet new people and stay connected, even when you're remote.\n"
+    + "If you're a team member, join the #kwikkonnect channel to join in the fun! I'll notify you of matches every week, held through 2min video calls on the KiwkKonnect video platform.\n"
+    + "If you're a team owner, use /match to begin the matching process!");
+})
+
+app.post('/match', async (req, res) => {
+    // getting channel members
     let members = await slack.conversations.members ({
-      channel: 'C012YGSHMFD',
+      channel: 'C012YGSHMFD', // #kiwkkonnect channel
     })
 
-    let channelMembers = members['members'];
-    let user;
-
+    // pairing up channel members
     let pairedMembers = {};
     let pairIndex, i;
-
     while (channelMembers.length != 0) {
       i = channelMembers.length - 1;
       pairIndex = Math.floor(Math.random()*i);
@@ -41,27 +34,22 @@ app.post('/', async (req, res) => {
       channelMembers.splice(pairIndex, 1);
     }
 
+    // sending match messages
+    let user;
     Object.keys(pairedMembers).forEach(async member => {
       user = await slack.users.profile.get({
         user: pairedMembers[member],
       })
       message = user['profile']['real_name'] + " is your KwikKonnect match! Here's how they're doing right now: " + user['profile']['status_text'] + "\n"
-      message += "Your 5min video call is scheduled for 12pm @ ourdomain.com/xyz. Have fun! :relaxed:"
+      message += "Your 5min video call is scheduled for 12pm at ourdomain.com/xyz.\n"
+      message += "Have fun! :relaxed:"
       await slack.chat.postMessage({
         channel: member,
         text: message,
       })
     })
 
-    // for (i=channelMembers.length - 1; i>= 0; i++) {
-    //   pairIndex = Math.floor(Math.random()*i);
-    //   pairedMembers[channelMembers[i]] = channelMembers[pairIndex];
-    //   pairedMembers[channelMembers[pairIndex]] = channelMembers[i];
-    //   channelMembers.splice(i, 1);
-    //   channelMembers.splice(pairIndex, 1);
-    // }
-
     res.send("Everyone in #kwikkonnect was matched up!")
 })
 
-app.listen(port, () => console.log('Example app listening at http://localhost:${port}'))
+app.listen(port, () => console.log('Slack backend app listening at http://localhost:${port}'))
