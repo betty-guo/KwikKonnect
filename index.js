1da00f4d3bb41374d3497263f6d574cd1473b596
Aphexis <stephxca@gmail.com>
2020-05-02 23:51:58 -0400

    sort of broken

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 const { WebClient } = require('@slack/web-api');
 
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.post('/', async (req, res) => {
@@ -26,36 +27,46 @@ app.post('/', async (req, res) => {
       channel: 'C012YGSHMFD',
     })
 
-    let channelMembers = members['members'];
-    let memberInfo = {}
-
-    channelMembers.forEach(async (value) => {
-      memberInfo[value] = await slack.users.profile.get({
-        user: value,
-      })
-      console.log(memberInfo[value]);
+    let members2 = await slack.conversations.members ({
+      channel: 'C012YGSHMFD',
     })
-    console.log(memberInfo);
+
+    let channelMembers = members['members'];
+    const testing = members2['members'];
+    let memberInfo = {}
+    let user;
+
     let pairedMembers = {};
     let pairIndex, i;
 
-    while (channelMembers.length != 0) {
-      i = channelMembers.length - 1;
-      pairIndex = Math.floor(Math.random()*i);
-      pairedMembers[channelMembers[i]] = channelMembers[pairIndex];
-      pairedMembers[channelMembers[pairIndex]] = channelMembers[i];
-      channelMembers.splice(i, 1);
-      channelMembers.splice(pairIndex, 1);
-    }
+    console.log(testing)
 
+    // while (channelMembers.length != 0) {
+    //   i = channelMembers.length - 1;
+    //   pairIndex = Math.floor(Math.random()*i);
+    //   pairedMembers[channelMembers[i]] = channelMembers[pairIndex];
+    //   pairedMembers[channelMembers[pairIndex]] = channelMembers[i];
+    //   channelMembers.splice(i, 1);
+    //   channelMembers.splice(pairIndex, 1);
+    // }
+
+    console.log(testing)
+    testing.forEach(async (value) => {
+      user = await slack.users.profile.get({
+        user: value,
+      })
+      // console.log(user)
+      memberInfo[value] = await user['profile'];
+      // console.log(memberInfo[value]);
+  
+    })
     Object.keys(pairedMembers).forEach(async member => {
-      message = memberInfo[pairedMembers[member]]['profile']['real_name'] + " is your KwikKonnect match!"
+      message = memberInfo[pairedMembers[member]]
         await slack.chat.postMessage({
         channel: member,
         text: message,
       })
     })
-
 
     // for (i=channelMembers.length - 1; i>= 0; i++) {
     //   pairIndex = Math.floor(Math.random()*i);
@@ -65,33 +76,6 @@ app.post('/', async (req, res) => {
     //   channelMembers.splice(pairIndex, 1);
     // }
 
-    // let pairings = {
-    // }
-
-    // slack.chat.postMessage({
-    //   channel: 'U012HJHFX9V',
-    //   text: message,
-    // })
-    // slack.chat.postMessage({
-    //   channel: 'U012HNJ6T63',
-    //   text: message,
-    // })
-    // slack.chat.postMessage({
-    //   channel: 'U012RJGS9QE',
-    //   text: message,
-    // })
-    // slack.chat.postMessage({
-    //   channel: 'U012YJ92ACT',
-    //   text: message,
-    // })
-    // slack.chat.postMessage({
-    //   channel: 'U0134FW95J8',
-    //   text: message,
-    // })
-    // slack.chat.postMessage({
-    //   channel: 'U013N5V6HLY',
-    //   text: message,
-    // })
     res.send(memberInfo)
 })
 
