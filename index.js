9d210483aa5f40616f1b02e839fae594a42d2280
Aphexis <stephxca@gmail.com>
2020-05-03 03:38:02 -0400

    created duration + frequency endpoints

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const app = express()
 const port = 3000
 const { WebClient } = require('@slack/web-api');
 
-// PUT AUTH TOKEN HERE
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.post('/info', async (req, res) => {
   // basic info about KiwkKonnect
-  res.send("Hey there, I'm KwikKonnect! I'm here to help you meet new people and stay connected, even when you're remote.\n"
+  res.send("Hey there, I'm Kwikbot! I'm here to help you meet new people and stay connected, even when you're remote.\n"
     + "If you're a team member, join the #kwikkonnect channel to join in the fun! I'll notify you of matches every week, held through 2min video calls on the KiwkKonnect video platform.\n"
     + "If you're a team owner, use /match to begin the matching process!");
+})
+
+app.post('/duration', async (req, res) => {
+  // set the duration for KwikKonnect calls
+  res.send("You got it! All KwikKonnect video calls will now be "  + req['body']['text'] + " in length!");
+})
+
+app.post('/frequency', async (req, res) => {
+  // set the frequency of KwikKonnect matches
+  res.send("On it! KwikKonnect matches will now occur every " + req['body']['text'] + "!");
 })
 
 app.post('/match', async (req, res) => {
@@ -22,14 +30,17 @@ app.post('/match', async (req, res) => {
       channel: 'C012YGSHMFD', // #kiwkkonnect channel
     })
 
+    let channelMembers = members['members']
+
     // pairing up channel members
     let pairedMembers = {};
     let pairIndex, i;
     while (channelMembers.length != 0) {
       i = channelMembers.length - 1;
       pairIndex = Math.floor(Math.random()*i);
-      pairedMembers[channelMembers[i]] = channelMembers[pairIndex];
-      pairedMembers[channelMembers[pairIndex]] = channelMembers[i];
+      let endpoint = Math.floor(Math.random()*1000); 
+      pairedMembers[channelMembers[i]] = [channelMembers[pairIndex], endpoint];
+      pairedMembers[channelMembers[pairIndex]] = [channelMembers[i], endpoint];
       channelMembers.splice(i, 1);
       channelMembers.splice(pairIndex, 1);
     }
@@ -38,10 +49,11 @@ app.post('/match', async (req, res) => {
     let user;
     Object.keys(pairedMembers).forEach(async member => {
       user = await slack.users.profile.get({
-        user: pairedMembers[member],
+        user: pairedMembers[member][0],
       })
+      
       message = user['profile']['real_name'] + " is your KwikKonnect match! Here's how they're doing right now: " + user['profile']['status_text'] + "\n"
-      message += "Your 5min video call is scheduled for 12pm at ourdomain.com/xyz.\n"
+      message += "Your 2min video call is scheduled for 12pm at ourdomain.com/" + pairedMembers[member][1] + ".\n"
       message += "Have fun! :relaxed:"
       await slack.chat.postMessage({
         channel: member,
